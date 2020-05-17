de6713fbe423becebc6193a13934a84bd6185e8d
Simon Perryman <sperryman15@gmail.com>
2020-05-17 13:14:48 +0100

    Developed the initial stages to randomise the board state

--- chessConfuser.js
+++ chessConfuser.js
@@ -7,9 +7,26 @@ const swap = (child1, child2) => {
   return child1, child2;
 }
 
+const randomisePieces = (pieces, numberOfRandomisations, colour) => {
+  sideCorrection = colour == 1 ? 15 : 0; 
+  while(numberOfRandomisations > 0) {
+    let position1 = sideCorrection + Math.floor(Math.random() * 16);
+    let position2 = sideCorrection + Math.floor(Math.random() * 16);
+
+    pieces[position1], pieces[position2] = swap(pieces[position1], pieces[position2]);
+    numberOfRandomisations--;
+  }
+}
+
 const chessConfuser = () => {
   const children = pieces.children;
-  children[0], children[1] = swap(children[0], children[1]);
+  let numberOfRandomisations = 15;
+
+  //Randomise whites pieces
+  randomisePieces(children, 15, 0);
+
+  //Randomise blacks pieces
+  randomisePieces(children, 15, 1);
 }
 
 const pieces = document.getElementsByClassName("pieces")[0];
